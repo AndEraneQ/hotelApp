e685a1763756bb242c735b2cbd1dabfab8415fab
Piotr Trojan <trojanpiotrek@interia.pl>
2024-05-31 17:56:38 +0200

    added addBooking method

--- HotelAppService/src/main/java/com/lakeSideHotel/model/Room.java
+++ HotelAppService/src/main/java/com/lakeSideHotel/model/Room.java
@@ -5,8 +5,11 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.cglib.core.Block;
 
 import java.math.BigDecimal;
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,11 +24,26 @@ public class Room {
     private String roomType;
     private BigDecimal roomPrice;
     private boolean isBooked = false;
+
+    @Lob
+    private Blob photo;
+    
     @OneToMany(fetch = FetchType.LAZY , cascade = CascadeType.ALL)
     private List<BookedRoom> bookings;
 
     public Room() {
         this.bookings = new ArrayList<>();
+    }
+
+    public void addBooking(BookedRoom booking){
+        if(bookings == null){
+            bookings = new ArrayList<>();
+        }
+        bookings.add(booking);
+        booking.setRoom(this);
+        isBooked=true;
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        booking.setBookingConfirmationCode(bookingCode);
     }
 
 
