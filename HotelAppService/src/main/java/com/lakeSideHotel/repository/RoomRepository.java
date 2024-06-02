7123973734e22391068b4cd3280b8f82bf2f8bd7
Piotr <trojanpiotrek@interia.pl>
2024-06-02 10:12:21 +0200

    create adding room funcionallity

--- HotelAppService/src/main/java/com/lakeSideHotel/repository/RoomRepository.java
+++ HotelAppService/src/main/java/com/lakeSideHotel/repository/RoomRepository.java
@@ -1,4 +1,9 @@
 package com.lakeSideHotel.repository;
 
-public interface RoomRepository {
+import com.lakeSideHotel.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room,Long> {
 }
